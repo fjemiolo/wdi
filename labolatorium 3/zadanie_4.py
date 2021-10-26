16fb159524641b63ff8f442fb4bc54bb384edc3c
jemiolo <filipj@student.agh.edu.pl>
2021-10-26 18:21:38 +0200

    dodaje zadanie 4

---
+++ labolatorium 3/zadanie_4.py
@@ -0,0 +1,19 @@
+imie = 'Filip'
+nazwisko = 'Jemioło'
+potrawa = 'pierogi'
+zwierzę = 'pies'
+wynik = 5/7
+
+print(imie)
+print(nazwisko)
+print(potrawa)
+print(zwierzę)
+print(wynik)
+
+
+print (f'imie: {imie}, nazwisko: {nazwisko}, ulubiona potrawa: {potrawa}, ulubione zwierze: {zwierzę}')
+
+print (round(wynik, 1))
+print (round(wynik, 3))
+print (round(wynik, 5))
+print (round(wynik, 10))
