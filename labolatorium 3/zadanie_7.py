aa32d60df047d08fec3ff2b02a14b63db2fd47d2
jemiolo <filipj@student.agh.edu.pl>
2021-10-26 19:10:15 +0200

    dodane zadanie 7

--- labolatorium 3/zadanie_7.py
+++ labolatorium 3/zadanie_7.py
@@ -1,2 +1,19 @@
 a = int(input("Podaj mniejszą z granic zakresu: "))
 b = int(input("Podaj większą z granic zakresu: "))
+avg = (a+b)/2
+x = -2.5 #pomocnicza zmienna
+if b - a > 20:
+    if avg % 2 == 0:
+        for i in range(7):
+            if i == 3:
+                continue
+            print (avg - 3 + i)
+
+    else:
+        while x != 3.5:
+            print (avg + x)
+            x += 1
+
+else:
+    for i in range(a, b+1):
+        print(i)
