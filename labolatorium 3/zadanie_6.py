1d8e5fe2ca572d4849ed0644fc3c436660c5c604
jemiolo <filipj@student.agh.edu.pl>
2021-10-26 18:46:24 +0200

    dodaje zadanie 6

---
+++ labolatorium 3/zadanie_6.py
@@ -0,0 +1,14 @@
+a = int(input('Wprowadź pierwszą liczbę: '))
+b = int(input('Wprowadź drugą liczbę: '))
+if a < 0 and b < 0:
+    print ("Obie liczby są mniejsze od 0 :(")
+    exit()
+if a > 0 and b > 0:
+    print(f'suma liczb: {a+b}, różnica liczb: {a-b}, iloczyn liczb: {a*b}, iloraz liczb: {a/b}, kwadraty liczb: {a*a}, {b*b}, pierwiastki kwadratowe: {a**(1/2)}, {b**(1/2)}')
+if a < 0:
+    print(f'suma liczb: {a + b}, różnica liczb: {a - b}, iloczyn liczb: {a * b}, iloraz liczb: {a / b}, kwadraty liczb: {abs(a) * abs(a)}, {b * b}, pierwiastki kwadratowe: {abs(a) ** (1 / 2)}, {b ** (1 / 2)}')
+if b < 0:
+    print(
+        f'suma liczb: {a + b}, różnica liczb: {a - b}, iloczyn liczb: {a * b}, iloraz liczb: {a / b}, kwadraty liczb: {a * a}, {b * b}, pierwiastki kwadratowe: {a ** (1 / 2)}, {abs(b) ** (1 / 2)}')
+if a*b == 10:
+    print ("Yay!")
