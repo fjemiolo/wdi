b5d064c6e87a79ae663f35519a18ec937d0c2a0f
fjemiolo <filipj@student.agh.edu.pl>
2021-10-29 17:49:19 +0200

    dodaje skończone zadanie 8

--- labolatorium 3/zadanie_8.py
+++ labolatorium 3/zadanie_8.py
@@ -1,3 +1,9 @@
 a = int(input('wprowadź wysokość choinki: '))
 
 for i in range(a):
+    if i == 0:
+        print(' ' * (a - 1) + '^')
+    else:
+        print(' ' * (a - i - 1) + '*' * (i * 2 + 1))
+
+print(' ' * (a - 1) + 'U')
