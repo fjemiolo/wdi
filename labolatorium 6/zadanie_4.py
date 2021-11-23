1da1a659cd023021535c5103c5e78d16af5fe06c
fjemiolo <92811937+fjemiolo@users.noreply.github.com>
2021-11-23 15:59:16 +0100

    Add files via upload
    Dodaje poprawione zadanie 4

--- labolatorium 6/zadanie_4.py
+++ labolatorium 6/zadanie_4.py
@@ -6,6 +6,9 @@ Należy sprawdzić przypadki użycia dla kilku wczytywanych liczb naturalnych.
 Należy obsłużyć wyjątek wczytania danej niebędącej liczbą naturalną.
 """
 
+class NegativeNumberError(Exception):
+    pass
+
 def A(n):
     return n * n + n + 1
 
@@ -18,39 +21,23 @@ def is_multiple(num):
         n += 1
     print(f"{num} is not a multiple")
 
-#for i in range (20):
-#    print (A(i))
+while True:
+    try:
+        num = int(input("Enter your number: "))
+        if type(num) != int:
+            raise ValueError
+        if num > 0:
+            break
+        else:
+            raise NegativeNumberError
+    except ValueError:
+        print("Your input should be an integer.")
+    except NegativeNumberError:
+        print("Your number should be natural.")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
+is_multiple(num)
 
-is_multiple(num1)
-is_multiple(num2)
-is_multiple(num3)
 
-"""
-maks = max(num1,num2,num3)
-h1 = h2 = h3 = 0
-n = 1
 
-while A(n) <= max(num1, num2, num3):
-    if num1 / A(n) == int(num1 / A(n)):
-        print (f"{num1} is a multiple of A({n}) = {A(n)}")
-        h1 = 1
-    if num2 / A(n) == int(num2 / A(n)):
-        print (f"{num2} is a multiple of A({n}) = {A(n)}")
-        h2 = 1
-    if num3 / A(n) == int(num3 / A(n)):
-        print (f"{num3} is a multiple of A({n}) = {A(n)}")
-        h3 = 1
-    n += 1
-
-if h1 == 0:
-    print (f"{num1} is not a multiple")
-if h2 == 0:
-    print (f"{num2} is not a multiple")
-if h3 == 0:
-    print (f"{num3} is not a multiple")
-"""
+        
 
