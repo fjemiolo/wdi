c0ff18b79091b6d4653daac66dd360b70b8d29f5
fjemiolo <filipj@student.agh.edu.pl>
2021-11-30 15:38:21 +0100

    dodaje zadanie 5

--- labolatorium 8/zadanie_6.py
+++ labolatorium 8/zadanie_6.py
@@ -19,12 +19,12 @@ for i in range(N):
 
 max_length = 0
 
-for i in range(N-1):
+for i in range(N - 1):
     sum = list[i]
     sum_idx = i
     length = 1
-    for j in range(i+1, N):
-        if list[j-1] < list[j]:
+    for j in range(i + 1, N):
+        if list[j - 1] < list[j]:
             sum += list[j]
             sum_idx += j
             length += 1
@@ -41,4 +41,3 @@ print(max_length)
 # [0, 1, 2, 3, 7, 9, 3, 7, 8, 9, 10, 11, 12, 13, 6, 8, 3, 314, 75, 96, 3] => 7 (7 -> 13)
 # [15, 12, 4, 8, 11, 17, 2, 1, 1, 2] => 0
 # [15, 12, 4, 8, 11, 17, 2, 1, 1, 2, 24] => 3 (1 -> 24)
-
---
+++ labolatorium 9/zadanie_5.py
@@ -0,0 +1,44 @@
+"""
+Zadanie 5 - lab 9
+Proszę napisać program wczytujący tablicę dwuwymiarową o ustalonym wymiarze n×n
+i wypełniający ją liczbami ciągu Fibonacciego po spirali.
+Wymiar tablicy powinien być definiowany przez użytkownika.
+"""
+
+
+def fibonacci_numbers(n):
+    F = [1] * (n)
+    for i in range(2, n):
+        F[i] = F[i - 1] + F[i - 2]
+    return F
+
+
+N = int(input("Enter size of your array: "))
+A = [[0 for _ in range(N)] for _ in range(N)]
+Fib = fibonacci_numbers(N * N)
+
+cnt = 0
+low = 0
+high = N - 1
+layers = int((N + 1) / 2)
+for i in range(layers):
+    for j in range(low, high + 1):  # left to right
+        A[i][j] = Fib[cnt]
+        cnt += 1
+    for j in range(low + 1, high + 1):  # up to down
+        A[j][high] = Fib[cnt]
+        cnt += 1
+    for j in range(high - 1, low - 1, -1):  # right to left
+        A[high][j] = Fib[cnt]
+        cnt += 1
+    for j in range(high - 1, low, -1):  # down to up
+        A[j][low] = Fib[cnt]
+        cnt += 1
+    low += 1
+    high -= 1
+#    for element in A:
+#       print(element)
+#    print("----------")
+
+for line in A:
+    print(line)
